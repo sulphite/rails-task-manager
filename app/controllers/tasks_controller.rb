84a9d1c44ee10e64fc0fd4698ca985710c8073bf
Janett Gerrard <mikanchik@gmail.com>
2023-02-21 14:14:08 +0000

    create new task works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,22 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def new
+    @task = Task.new # we need this to instantiate the form
+  end
+
   def create
+    # we create and save the task from the params
+    # after we send the params to a different method for safety
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
   end
 end
