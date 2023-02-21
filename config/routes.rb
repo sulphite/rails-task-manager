1bb29d173b4df3ee07001dd2ea49cfd932fca639
Janett Gerrard <mikanchik@gmail.com>
2023-02-21 13:22:44 +0000

    index and show working

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/tasks", to: "tasks#index"
+  # get "/new", to: "tasks#create"
+
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  # the as: task is very important for reasons that I will explain later
 end
