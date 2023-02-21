979b712c123b962e1cf0997857bc91f5f7bddd06
Janett Gerrard <mikanchik@gmail.com>
2023-02-21 14:52:43 +0000

    delete works

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get "/tasks", to: "tasks#index"
   get "/new", to: "tasks#new"
   post "tasks", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 
-  get "/tasks/:id", to: "tasks#show", as: "task"
+  get "/tasks/:id", to: "tasks#show", as: :task
   # the as: task is very important for reasons that I will explain later
 end
